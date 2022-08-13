5cbc39c8464d498c75764c25718d0500cbf7ba91
Sajib <contact2sajib@gmail.com>
2022-08-13 16:02:57 +0600

    sidebar widget post and categories

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,14 +1,16 @@
-import React, {useContext} from 'react'
-
+import React, { useContext, useState, useEffect } from 'react'
 import Link from 'next/link'
 
-const categories = [
-    {name: 'React', slug: 'react'},
-    {name: 'Node', slug: 'node'},
-    {name: 'GraphQL', slug: 'graphql'}
-]
+import { getCategories } from '../services'
+
 
 const Header = () => {
+    const [categories, setCategories] = useState([])
+    
+    useEffect(() =>{
+      getCategories().then((newCategories) => setCategories(newCategories))
+    },[])
+    
   return (
     <div className="contianer mx-auto px-10 mb-8">
         <div className="border-b w-full inline-block border-blue-400 py-8">
